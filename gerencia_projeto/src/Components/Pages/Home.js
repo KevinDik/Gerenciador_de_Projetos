ba75b4fe36fae077d16bc21b52a6e33fad970955
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-09-23 10:17:40 -0300

    Adicionado formulário de novo projeto
    Inserido algumas funcionalidades, componentes, botões e formulários de interação para adição de novos projetos

--- gerencia_projeto/src/Components/Pages/Home.js
+++ gerencia_projeto/src/Components/Pages/Home.js
@@ -1,4 +1,16 @@
+import styles from './Home.module.css'
+
+import savings from '../../img/savings.svg'
+import LinkButton from '../Layouts/LinkButton'
+
 function Home() {
-    return (<h1>Home</h1>)
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>gerenciador de projetos</span></h1>
+            <p>Comece a gerenciar os seus projetos</p>
+            <LinkButton to="/newproject" text="Criar Projeto"/>
+            <img src={savings} alt="Gerenciador"/>
+        </section>
+    )
 } 
 export default Home 
--- gerencia_projeto/src/Components/Pages/NewProject.js
+++ gerencia_projeto/src/Components/Pages/NewProject.js
@@ -1,4 +1,13 @@
+import ProjectForm from '../Project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
-    return (<h1>New Project</h1>)
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar novo projeto</h1>
+            <p>Adicione seu projeto primeiramente para depois inserir seus serviços</p>
+            <ProjectForm btnText="Criar Projeto"/>
+        </div>
+    )
 } 
 export default NewProject
