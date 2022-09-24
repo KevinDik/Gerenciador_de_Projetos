388d308cb30ff4c29012d4851609c6bedb8747ba
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-09-24 16:54:49 -0300

    Criado cards Meus projects
    Adicionado projetos no link meus projetos

--- gerencia_projeto/src/Components/Pages/Projects.js
+++ gerencia_projeto/src/Components/Pages/Projects.js
@@ -2,17 +2,35 @@ import Messages from '../Layouts/Messages'
 import {useLocation} from 'react-router-dom'
 import Container from '../Layouts/Container'
 import LinkButton from '../Layouts/LinkButton'
+import ProjectCard from '../Project/ProjectCard'
+import { useState, useEffect } from 'react'
 
 import styles from './Projects.module.css'
 
 
 function Projects() {
 
+    const [projects, setProjects] = useState([])
+    
+
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then((resp) => resp.json())
+        .then(data => {
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className={styles.project_container}>
@@ -22,7 +40,16 @@ function Projects() {
             </div>
             {message && <Messages type="success" msg={message}/>}
             <Container customClass="start">
-                <p>Meus projeto</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard 
+                        name={project.name}
+                        id={project.id}
+                        budget={project.budget}
+                        category={project.category.name}
+                        key={project.key}
+                        />
+                ))}
             </Container>
         </div>
     )
