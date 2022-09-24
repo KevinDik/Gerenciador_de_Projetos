c6f90e0c958aec6423e55a4c1f1b07a584c63c3c
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-09-24 14:43:58 -0300

    Inserindo valores no DataBase
    inserido valores via API no banco de dados

--- gerencia_projeto/src/Components/Pages/NewProject.js
+++ gerencia_projeto/src/Components/Pages/NewProject.js
@@ -1,12 +1,37 @@
 import ProjectForm from '../Project/ProjectForm'
 import styles from './NewProject.module.css'
+import {useHistory} from 'react-router-dom'
 
 function NewProject() {
+
+    const history = useHistory()
+
+    function createPost(project) {
+        //inicializa o project e os serviços
+
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            history.push('/projects', {message: 'Projeto criado com sucesso!'})
+        })
+        .catch((err) => console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar novo projeto</h1>
             <p>Adicione seu projeto primeiramente para depois inserir seus serviços</p>
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     )
 } 
