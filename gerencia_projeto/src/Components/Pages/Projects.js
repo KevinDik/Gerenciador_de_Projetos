c1eae00bc627f08779eb5666dc0c2082300167b6
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-09-24 15:56:21 -0300

    Configurado estilo do Dashboard
    Adicionado botões e funcionalidades que englobam todo o projeto

--- gerencia_projeto/src/Components/Pages/Projects.js
+++ gerencia_projeto/src/Components/Pages/Projects.js
@@ -1,5 +1,31 @@
+import Messages from '../Layouts/Messages'
+import {useLocation} from 'react-router-dom'
+import Container from '../Layouts/Container'
+import LinkButton from '../Layouts/LinkButton'
+
+import styles from './Projects.module.css'
+
+
 function Projects() {
-    return <h1>Projects</h1>
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
+            {message && <Messages type="success" msg={message}/>}
+            <Container customClass="start">
+                <p>Meus projeto</p>
+            </Container>
+        </div>
+    )
 }
 
 export default Projects
